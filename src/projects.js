f68bf63098a4335f5f4559e08203df11f2656b82
Rylie jo <144120933+bluelemon0516@users.noreply.github.com>
2023-11-17 22:44:12 +0900

    JS project filter

--- src/projects.js
+++ src/projects.js
@@ -1,2 +1,31 @@
 'use strict';
 
+// 프로젝트 필터링 관련 로직 처리
+const categories = document.querySelector('.categories');
+const projects = document.querySelectorAll('.project');
+const projectsContainer = document.querySelector('.projects');
+categories.addEventListener('click', (event) => {
+    const filter = event.target.dataset.category;
+    if(filter == null) {
+        return;
+    }
+
+    // active 메뉴 설정
+    const active = document.querySelector('.category__selected');
+    active.classList.remove('category__selected');
+    event.target.classList.add('category__selected');
+
+
+    // 프로젝트 필터링 로직
+    projectsContainer.classList.add('anim-out');
+    projects.forEach((project) => {
+        if (filter === 'all' || filter === project.dataset.type) {
+            project.style.display = 'block';
+        } else {
+            project.style.display = 'none';
+        }
+    });
+    setTimeout( () => {
+        projectsContainer.classList.remove('anim-out');
+    }, 250);
+});
